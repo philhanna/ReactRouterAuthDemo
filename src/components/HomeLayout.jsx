dbf5b5b1aae2986f46a42fd101bcd123738d68c0
Phil Hanna <ph1204@gmail.com>
2023-07-03 19:32:02 -0400

    Using <Outlet />

---
+++ src/components/HomeLayout.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../hooks/useAuth"
+
+export const HomeLayout = () => {
+    const { user } = useAuth()
+    if (user) {
+        return <Navigate to="/dashboard/profile" />
+    }
+
+    return (
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+            </nav>
+            <Outlet />
+        </div>
+    )
+}
