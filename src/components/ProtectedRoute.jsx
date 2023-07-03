7b32d88d9983319af49f607a10c460f0f422c45f
Phil Hanna <ph1204@gmail.com>
2023-07-03 11:23:41 -0400

    Fixed typo in import name

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigage } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
 
 export const ProtectedRoute = ({ children }) => {
