9e638aca1d7312b8a1c0f9e34e16afd75cfa37f6
Phil Hanna <ph1204@gmail.com>
2023-07-03 19:24:49 -0400

    Using nested routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import { Routes, Route } from "react-router-dom"
+import { ProtectedLayout } from "./components/ProtectedLayout"
+import { HomeLayout } from "./components/HomeLayout"
 import { LoginPage } from "./pages/Login"
 import { HomePage } from "./pages/Home"
 import { RegisterPage } from "./pages/Register"
 import { ProfilePage } from "./pages/Profile"
 import { SettingsPage } from "./pages/Settings"
-import { ProtectedRoute } from "./components/ProtectedRoute"
 
 export default function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/register" element={<RegisterPage />} />
-			<Route
-				path="/profile"
-				element={
-					<ProtectedRoute>
-						<ProfilePage />
-					</ProtectedRoute>
-				}
-			/>
-			<Route
-				path="/settings"
-				element={
-					<ProtectedRoute>
-						<SettingsPage />
-					</ProtectedRoute>
-				}
-			/>
+			<Route element={<HomeLayout />}>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+			</Route>
+			<Route path="/dashboard" element={<ProtectedLayout />}>
+				<Route path="profile" element={<ProfilePage />} />
+				<Route path="settings" element={<SettingsPage />} />
+			</Route>
 		</Routes>
 	)
 }
