9b18ad45b2c34f8c59be3f8586fc199254ac2bef
Phil Hanna <ph1204@gmail.com>
2023-07-03 21:30:13 -0400

    createRoutesFromElements()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { Routes, Route } from "react-router-dom"
+import {
+	Routes,
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom"
 import { ProtectedLayout } from "./components/ProtectedLayout"
 import { HomeLayout } from "./components/HomeLayout"
 import { LoginPage } from "./pages/Login"
 import { HomePage } from "./pages/Home"
-import { RegisterPage } from "./pages/Register"
+// import { RegisterPage } from "./pages/Register"
 import { ProfilePage } from "./pages/Profile"
 import { SettingsPage } from "./pages/Settings"
 
-export default function App() {
-	return (
-		<Routes>
+export const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
 			<Route element={<HomeLayout />}>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/login" element={<LoginPage />} />
@@ -19,6 +24,6 @@ export default function App() {
 				<Route path="profile" element={<ProfilePage />} />
 				<Route path="settings" element={<SettingsPage />} />
 			</Route>
-		</Routes>
+		</>
 	)
-}
+)
