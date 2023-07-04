9ef7b9cdb6f957158e46f22ac3a8f179294ef264
Phil Hanna <ph1204@gmail.com>
2023-07-03 22:07:18 -0400

    useLoaderData

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import {
-	Routes,
 	Route,
 	createBrowserRouter,
 	createRoutesFromElements,
+	defer,
 } from "react-router-dom"
 import { ProtectedLayout } from "./components/ProtectedLayout"
 import { HomeLayout } from "./components/HomeLayout"
 import { LoginPage } from "./pages/Login"
 import { HomePage } from "./pages/Home"
-// import { RegisterPage } from "./pages/Register"
 import { ProfilePage } from "./pages/Profile"
 import { SettingsPage } from "./pages/Settings"
+import { AuthLayout } from "./components/AuthLayout"
+
+const getUserData = () =>
+	new Promise((resolve) =>
+		setTimeout(() => {
+			const user = window.localStorage.getItem("user")
+			resolve(user)
+		}, 3000)
+	)
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
-		<>
+		<Route
+			element={<AuthLayout />}
+			loader={() => defer({ userPromise: getUserData() })}
+		>
 			<Route element={<HomeLayout />}>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/login" element={<LoginPage />} />
@@ -24,6 +35,6 @@ export const router = createBrowserRouter(
 				<Route path="profile" element={<ProfilePage />} />
 				<Route path="settings" element={<SettingsPage />} />
 			</Route>
-		</>
+		</Route>
 	)
 )
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, RouterProvider } from "react-router-dom"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { router } from "./App"
 
-import App from "./App"
+const theme = createTheme({
+    palette: {
+      primary: { main: "#3a34d2" }
+    }
+  })
 
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</StrictMode>
 )
